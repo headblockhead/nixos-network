f176b4da6e29135b2514d9c3740299f99599dfa5
Edward Hesketh <inbox@edwardh.dev>
2025-05-26 01:03:45 +0100

    feat: add yubikey encryption to edward-laptop-01

--- systems/edward-laptop-01/hardware.nix
+++ systems/edward-laptop-01/hardware.nix
@@ -11,5 +11,30 @@
 
   networking.useDHCP = lib.mkDefault true;
 
+  boot.initrd = {
+    # Required to open the EFI partition and Yubikey
+    kernelModules = [ "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
+
+    luks = {
+      yubikeySupport = true;
+      devices."encrypted" = {
+        device = "/dev/sda3";
+        yubikey = {
+          slot = 2;
+          twoFactor = true;
+          gracePeriod = 30; # Time in seconds to wait for Yubikey to be inserted
+          keyLength = 64;
+          saltLength = 16;
+          storage = {
+            device = "/dev/sda1";
+            fsType = "vfat";
+            path = "/crypt-storage/default";
+          };
+        };
+      };
+    };
+  };
+
+
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
